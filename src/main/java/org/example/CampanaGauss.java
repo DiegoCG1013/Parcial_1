b317d3707863a875d18b0d5ff71c9a2d846e54a2
Diego <diegocarrasco1013@gmail.com>
2023-10-16 12:49:08 +0200

    metodo para mostrar la campana

--- src/main/java/org/example/CampanaGauss.java
+++ src/main/java/org/example/CampanaGauss.java
@@ -1,4 +1,31 @@
 package org.example;
 
 public class CampanaGauss {
+
+    int tamanyoContenedor = 10;
+    Boolean[][] contenedor = new Boolean[tamanyoContenedor][tamanyoContenedor];
+
+    public CampanaGauss() {
+        for (int i = 0; i < contenedor.length; i++) {
+            for (int j = 0; j < contenedor.length; j++) {
+                contenedor[i][j] = false;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        String aux = "";
+        for (int i = 0; i < contenedor.length; i++) {
+            for (int j = 0; j < contenedor.length; j++) {
+                if (contenedor[i][j]) {
+                    aux += " o ";
+                } else {
+                    aux += "   ";
+                }
+            }
+            aux += "\n";
+        }
+        return aux;
+    }
 }
